52002ac125d8bc8bad09b146c8398205fbd6ea5c
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-02 18:35:23 +0600

    Edit in gitignore file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+// const port = process.env.PORT || 3000;
 const port = 3000;
 
+// Define routes
 app.get("/", (req, res) => {
-	res.send("Hello World Test!");
+	res.send("Hello, World!");
 });
 
+// Start server
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
